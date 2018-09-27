ef1f1bc5a1b55f84f5f1e24b4be945de1368d927
William Durand <will+git@drnd.me>
2018-09-27 17:53:18 +0200

    Always return a Promise in background script

--- src/background.js
+++ src/background.js
@@ -1,19 +1,23 @@
 browser.runtime.onMessage.addListener(({ from, origin }) => {
-  if (from === 'popup') {
-    const options = {};
+  return new Promise((resolve, reject) => {
+    if (from === 'popup') {
+      const options = {};
 
-    // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1450649
-    if (origin === 'https://addons.mozilla.org') {
-      // eslint-disable-next-line no-param-reassign
-      origin = `https://cors-anywhere.herokuapp.com/${origin}`;
-      options.headers = new Headers({ 'x-requested-with': 'amo-info' });
+      // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1450649
+      if (origin === 'https://addons.mozilla.org') {
+        console.debug('replacing "origin" because of a FF restriction');
+
+        // eslint-disable-next-line no-param-reassign
+        origin = `https://cors-anywhere.herokuapp.com/${origin}`;
+        options['headers'] = new Headers({ 'x-requested-with': 'amo-info' });
+      }
+
+      return fetch(`${origin}/__frontend_version__`, options)
+        .then((response) => response.json())
+        .then((payload) => resolve({ type: 'success', payload }))
+        .catch((error) => resolve({ type: 'error', error }));
     }
 
-    return fetch(`${origin}/__frontend_version__`, options)
-      .then((response) => response.json())
-      .then((payload) => Promise.resolve({ type: 'success', payload }))
-      .catch((error) => Promise.resolve({ type: 'error', error }));
-  }
-
-  return Promise.resolve({ type: 'unknown' });
+    resolve({ type: 'unknown' });
+  });
 });
