e03985dd0a023817007b97b0d17903d934784754
William Durand <will+git@drnd.me>
2019-10-17 11:16:59 +0200

    Fix prettier

--- scripts/remove-evals.js
+++ scripts/remove-evals.js
@@ -4,10 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const BUNDLE_DIR = path.join(__dirname, '../dist');
-const bundles = [
-  'background.js',
-  'popup/popup.js',
-];
+const bundles = ['background.js', 'popup/popup.js'];
 
 const evalRegexForProduction = /;([a-z])=function\(\){return this}\(\);try{\1=\1\|\|Function\("return this"\)\(\)\|\|\(0,eval\)\("this"\)}catch\(t\){"object"==typeof window&&\(\1=window\)}/g;
 const evalRegexForDevelopment = /;\\r\\n\\r\\n\/\/ This works in non-strict mode(?:.){1,304}/g;
@@ -17,14 +14,17 @@ const removeEvals = (file) => {
 
   return new Promise((resolve, reject) => {
     fs.readFile(file, 'utf8', (err, data) => {
-      if(err) {
+      if (err) {
         reject(err);
         return;
       }
 
-      const regex = process.env.NODE_ENV === 'production' ? evalRegexForProduction : evalRegexForDevelopment;
+      const regex =
+        process.env.NODE_ENV === 'production'
+          ? evalRegexForProduction
+          : evalRegexForDevelopment;
 
-      if(!regex.test(data)) {
+      if (!regex.test(data)) {
         reject(`No CSP specific code found in ${file}.`);
         return;
       }
@@ -32,7 +32,7 @@ const removeEvals = (file) => {
       data = data.replace(regex, '=window;');
 
       fs.writeFile(file, data, (err) => {
-        if(err) {
+        if (err) {
           reject(err);
           return;
         }
@@ -44,7 +44,7 @@ const removeEvals = (file) => {
 };
 
 const main = () => {
-  bundles.forEach(bundle => {
+  bundles.forEach((bundle) => {
     removeEvals(path.join(BUNDLE_DIR, bundle))
       .then(() => console.info(`Bundle ${bundle}: OK`))
       .catch(console.error);
